af790a3d4a671b63aac4b6eabcd0fa2f6f021aea
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-10-18 19:21:17 +0700

    menentukan posisi tulisan yang kita inginkan

---
+++ app/src/main/java/com/example/tugasbasiccompose/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.tugasbasiccompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugasbasiccompose.ui.theme.TugasBasicComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TugasBasicComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicColumn(modifier =Modifier.padding(innerPadding)
+
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BasicColumn(modifier: Modifier = Modifier)  {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 30.dp)
+    ){
